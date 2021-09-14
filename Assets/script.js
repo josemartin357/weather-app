46f0a1aa40d1a78cb525e576fb2ce1f5301ee9fe
josemartin357 <83382332+josemartin357@users.noreply.github.com>
2021-09-14 12:04:02 -0500

    Tested fetch from API; established variables and tested basic innerHTML property in the name, temperature and description values. Result = fetching works.

--- Assets/script.js
+++ Assets/script.js
@@ -26,8 +26,6 @@ button.addEventListener('click', function() {
 .catch(err => alert("Wrong city name"))
 })
 
-
-
 // Your Task
 // Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
 // Use the OpenWeather One Call API to retrieve weather data for cities. Read through the documentation for setup and usage instructions. You will use localStorage to store any persistent data. For more information on how to work with the OpenWeather API, refer to the Full-Stack Blog on how to use API keys.
